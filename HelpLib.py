271dd169e6f169903afd338eee90517d71fee4d5
PortikAbel <portikabel@yahoo.com>
2022-05-17 15:39:40 +0300

    deleted pycache

--- HelpLib.py
+++ HelpLib.py
@@ -1,16 +1,11 @@
 try:
-    from ast import keyword
     from robot.api.deco import keyword
-    from selenium import webdriver
     from webdriver_manager.chrome import ChromeDriverManager
     ROBOT = False
 except Exception:
     ROBOT = False
 
 class HelpLib():
-
-    def __init__(self) -> None:
-        pass
 
     @keyword("get chromedriver path")
     def get_chromedriver_path(self):
